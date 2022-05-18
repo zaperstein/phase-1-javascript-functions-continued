e7ce05d84a5b57a606c79c1b1d04299d48b85561
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-18 10:14:06 -0500

    commit tree 61c087289ab0567a77d7e4e09a1eff6ea4edb499

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return (`This Saturday, I want to ${activity}!`)
+};
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = '*') {
+  return function(adjective='special') {
+    return `You are ${flair}${adjective}${flair}!`
+    
+  }
+}
+
+
+//https://learning.flatironschool.com/courses/5192/assignments/185934?module_item_id=413144
